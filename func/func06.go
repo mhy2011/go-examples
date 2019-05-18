a3f2655b5a92c89867a5ce2fe392473c09922046
mhy2011 <mhy2011@163.com>
2019-05-18 22:24:51 +0800

    update code

--- func/func06.go
+++ func/func06.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	callback(5, 6, Add)
+	callback(5, 6, Sub)
 }
 
 func Add(a, b int) {
 	fmt.Printf("The sum of %d and %d is: %d\n", a, b, a+b)
+}
+
+func Sub(a, b int) {
+	fmt.Printf("The diff of %d and %d is: %d\n", a, b, a-b)
 }
 
 func callback(x, y int, f func(int, int)) {
