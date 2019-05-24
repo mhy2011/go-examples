63dc43b66892efb4c1194ce9c8065c952be69cd2
mhy2011 <mhy2011@163.com>
2019-05-24 17:41:17 +0800

    add method for rectangle

--- struct/struct01.go
+++ struct/struct01.go
@@ -8,10 +8,18 @@ func main() {
 
 	rect := Rectangle{length: 10, width: 5}
 	fmt.Println("rect=", rect)
+
+	fmt.Printf("Area for rect is %d\n", rect.Area())
 }
 
 // 定义长方形结构体
 type Rectangle struct {
 	length uint32 //长度
 	width  uint32 //宽度
+}
+
+//定义求长方形面积的方法
+func (rect Rectangle) Area() (area uint32) {
+	area = rect.length * rect.width
+	return
 }
